6aaeb375e975685052e68518e005d0f3be172629
Efandio <efandio904@gmail.com>
2025-04-09 11:19:40 +0700

    contact form using google form

--- src/components/page/Contact.tsx
+++ src/components/page/Contact.tsx
@@ -2,8 +2,8 @@
 
 const Contact = () => {
   return (
-    <div>
-      contact
+    <div className="overflow-x-hidden lg:-translate-x-2">
+      <iframe className="w-[90vw] lg:w-[50vw]" src="https://docs.google.com/forms/d/e/1FAIpQLSfukTv3Tz99yod4PQjOkjoicApkRoBWXmpscUIZDt-bm3w6dw/viewform?embedded=true" width="640" height="686" frameBorder="0" marginHeight="0" marginwidth="0">Loading…</iframe>
     </div>
   )
 }
